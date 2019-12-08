b1468d7824e8a7824c2ebb6476ae8fc4aeb9e560
zachwooding <zachwooding@gmail.com>
2019-12-07 17:44:17 -0700

    added water physics

---
+++ CameraDrag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraDrag : MonoBehaviour
+{
+    public Transform target;
+    int degrees = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0))
+        {
+
+            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * degrees);
+            transform.RotateAround (target.position, Vector3.right, Input.GetAxis ("Mouse Y")* degrees);
+        }
+    }
+}
+
+
